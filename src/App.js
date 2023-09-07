de6088288cc6e7436ef20fd2f1c8cc9a5b4d09a8
mohamed-4rarh <mainmohedadham@gmail.com>
2023-09-07 23:39:32 +0300

    useLocalStorage and fetch api

--- src/App.js
+++ src/App.js
@@ -4,18 +4,50 @@ import {marked} from 'marked'
 import { useLocalStorage } from './useLocalStorage';
 
 const App = () => {
-  const [code, setCode] = useLocalStorage('mark_down', '## Hello')
-  const [compiled, setCompiled] = useState('<h2 id="hello">Hello</h2>')
-  const [hide, hidePreview] = useState(true)
+  const [code, setCode] = useLocalStorage('mark_down', '## Hello') // to push and retrive data from the localStorage
+  const [compiled, setCompiled] = useLocalStorage('compiled_preview', '<h2 id="hello">Hello</h2>') // storing the data in the localStorage after parsing to html elements
+  const [docs, setDocs] = useState([]) 
+  
+  //states to handling the "<textarea />" content
+  const [markDown, setMarkDown] = useState(true)
+  const [preview, setPreview] = useState(false)
+  const [doc, setDoc] = useState(false)
+
+  // useEffect(() => {
+  //   async function getDocs () {
+  //     try {
+  //       const response = await fetch('https://www.markdownguide.org/api/v1/basic-syntax.json')
+  //       setDocs(response.json())
+  //     } catch (error) {
+  //       console.error(error)
+  //     }
+  //   }
+  //   getDocs()
+  // }, [])
+  //
+  // the previous block of code is the async function fetching the data from the api endpoint but it keeps giving me the network error with code 200
+  // means the requist state is "OK and success" but there is something blocking me out or rejecting the requist to be fulfilled
+  // I tested the endpoints on "postman" and it's responsing with the data but doesn't work on my machine
+  //i found in some articles that it's maybe blocked because of the localhost:3000
+  //but the rest of the project is too much easy, 
+  //get the data from api and arranging it to html elements with some styles with css it will be greate,
 
   const openMD = () => {
-    console.log(0)
-    hidePreview(true)
+    setMarkDown(true)
+    setPreview(false)
+    setDoc(false)
   }
 
   const openPreview = () => {
-    console.log(0)
-    hidePreview(false)
+    setMarkDown(false)
+    setPreview(true)
+    setDoc(false)
+  }
+
+  const openDocs = () => {
+    setMarkDown(false)
+    setPreview(false)
+    setDoc(true)
   }
 
   const handleChange = (e) => {
@@ -30,16 +62,11 @@ const App = () => {
         <div className="btns">
           <button onClick={openMD} className="btn">MarkDown</button>
           <button onClick={openPreview}>Preview</button>
+          <button onClick={openDocs}>DOCS</button>
         </div>
-        {
-        hide ? 
-          <div>
-            <textarea onChange={handleChange} value={code}/>
-          </div> : 
-          <div>
-            <textarea value={compiled}/>
-          </div>
-        }
+        {markDown && <div><textarea onChange={handleChange} value={code} /></div> }
+        {preview && <div><textarea value={compiled} /></div> }
+        {doc && <div><textarea value={docs} /></div> }
       </div>
     </>
   )
--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -2,13 +2,14 @@ import { useState, useEffect } from "react";
 
 export function useLocalStorage(key, initialValue) {
     const [value, setValue] = useState(() => {
-        return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : initialValue
-    })
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value))
-    }, [value, key])
+        return JSON.parse(localStorage.getItem(key)) !== null ? JSON.parse(localStorage.getItem(key)) : initialValue;
+    });
 
-    return [value, setValue]
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [value, key]);
+
+    return [value, setValue];
 }
 
 //value => is the localStorage data inside the given 'key'
